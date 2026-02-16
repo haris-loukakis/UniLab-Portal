49e92ad138c97f26161121e3c4243559240e6907
Χάρης Λουκάκης <harisloukakis@MacBook-Air-Chares.local>
2026-02-16 14:44:39 +0200

    Upload project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-white text-5xl font-bold">
+        Λειτουργεί!
+      </h1>
+    </div>
+  );
+}
